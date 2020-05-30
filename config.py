e894dace47dc4b1d5df0d2d03bd7deb97a03c952
Maisie Zhang <maisie.zhang@intel.com>
2020-05-30 22:00:11 +0800

    Inital

---
+++ config.py
@@ -0,0 +1,75 @@
+# backgorund colors
+theme_color = {
+'Default': '#000000.#FFFFFF',
+'Greygarious': '#83406A.#D1D4D1',
+'Aquamarine': '#5B8340.#D1E7E0',
+'Bold Beige': '#4B4620.#FFF0E1',
+'Cobalt Blue': '#ffffBB.#3333aa',
+'Olive Green': '#D1E7E0.#5B8340',
+'Night Mode': '#FFFFFF.#000000',
+}
+
+#ICONS
+ICONS = ['new', 'open', 'save', 'cut', 'copy', 'paste',
+'undo', 'redo', 'find']
+
+#Format function
+Format = ['bold', 'italic', 'underline', 'strike', 'font-color', 
+'highlight', 'align-center', 'align-justify', 'align-left']
+
+#shot cut
+shotcuts = {
+'<Control-n>': 'new',
+'<Control-N>': 'new',
+
+'<Control-o>': 'open_file',
+'<Control-O>': 'open_file',
+
+'<Control-s>': 'save',
+'<Control-S>': 'save',
+
+'<Control-Shift-s>': 'save_as',
+'<Control-Shift-S>': 'save_as',
+
+'<Control-r>': 'rename',
+'<Control-R>': 'rename',
+
+'<Alt-F4>': 'close',
+'<Alt-F4>': 'close',
+
+'<Control-x>': 'cut',
+'<Control-X>': 'cut',
+
+'<Control-c>': 'copy',
+'<Control-C>': 'copy',
+
+'<Control-p>': 'paste',
+'<Control-P>': 'paste',
+
+'<Control-a>': 'select_all',
+'<Control-A>': 'select_all',
+
+'<Control-h>': 'about',
+'<Control-H>': 'about',
+
+'<Control-f>': 'find_text',
+'<Control-F>': 'find_text',
+
+'<Control-Shift-i>': 'italic',
+'<Control-Shift-I>': 'italic',
+
+'<Control-b>': 'bold',
+'<Control-B>': 'bold',
+
+'<Control-u>': 'underline',
+'<Control-U>': 'underline',
+
+'<Control-Shift-l>': 'align_left',
+'<Control-Shift-L>': 'align_left',
+
+'<Control-Shift-r>': 'align_right',
+'<Control-Shift-R>': 'align_right',
+
+'<Control-Shift-c>': 'align_center',
+'<Control-Shift-C>': 'align_center'
+}
